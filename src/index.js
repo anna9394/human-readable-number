21acae2397d1f924668bddcb55c45a00b9de0911
Anna <annaandreevna90394@gmail.com>
2021-02-10 12:58:02 +0300

    add function implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
+const a = ['', 'one ', 'two ', 'three ', 'four ', 'five ', 'six ', 'seven ', 'eight ', 'nine ', 'ten ', 'eleven ', 'twelve ', 'thirteen ', 'fourteen ', 'fifteen ', 'sixteen ', 'seventeen ', 'eighteen ', 'nineteen ']
+const b = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+
+const regex = /^(\d{1})(\d{2})$/
+
+const newNum = (n) => a[Number(n)]
+const newNumber = (n) => b[n[0]] + ' ' + a[n[1]]
+
 module.exports = function toReadable (number) {
-  
+  const num = Number(number)
+  if (num === 0) {
+    return 'zero'
+  } 
+
+  const numStr = num.toString()
+
+  const [, n1, n2] = ('000' + numStr).substr(-3).match(regex) 
+
+  let str = ''
+
+  str += n1 != 0 ? newNum(n1) + 'hundred ' : ''
+  str += n2 != 0 ? (newNum(n2) || newNumber(n2)) : ''
+
+  return str.trim() 
 }
+
+  
+  
